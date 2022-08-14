5aea6cac896ca1ddf020a0820a9f45fd1d2456be
Yana Demidovich <demyanvo1990@gmail.com>
2022-08-14 21:36:21 +0300

    Сохранила задачу 1

--- Lection5/Seminar7/Program.cs
+++ Lection5/Seminar7/Program.cs
@@ -1,8 +1,9 @@
-﻿// Задача 1. Задать двумерный массив m на n и заполнить случайными целыми числами.
+﻿// Задать двумерный массив m на n и заполнить случайными целыми числами.
 // Двумерный массив - матрица
 // "," определяет кол-во индексов элементов на вход, их будет 2
 
-int[,] CreateRandom2dArray(int rows, int columns, int minValue, int maxValue) // метод определения матрицы массива
+/* int[,] CreateRandom2dArray(int rows, int columns, int minValue, int maxValue) // метод определения кол-ва строк и столбцов
+{                                                               // и заполнения числами
 {
     int[,] newArray = new int[rows, columns];
 
@@ -13,9 +14,9 @@ int[,] CreateRandom2dArray(int rows, int columns, int minValue, int maxValue) //
     return newArray;
 }
 
-void Show2dArray(int[,] array) // метод заполения случайными целыми числами строки и столбцы
+void Show2dArray(int[,] array) // метод вывода на экран полученного 2D (двумерного) массива
 {
-    for(int i = 0; i < array.GetLength(0); i++)
+    for(int i = 0; i < array.GetLength(0); i++) // GetLength - получить длину
     {
         for(int j = 0; j < array.GetLength(1); j++)
             Console.Write(array[i,j] + " ");
@@ -30,5 +31,46 @@ Console.WriteLine("Введите кол-во колонок ");
 int n = Convert.ToInt32(Console.ReadLine());
 
 int[,] myArray = CreateRandom2dArray(m, n, 1, 9);
-Show2dArray(myArray);
+Show2dArray(myArray); */
+
+// Задача 1. Задайте двумерный массив размера m на n  
+// Каждый элемент в массиве находится по формуле: Array(ij) = i + j.
+// Выведите полученный массив на экран.
+
+/* int[,] CreateRandom2dArray(int rows, int columns, int minValue, int maxValue) // метод определения кол-ва строк и столбцов
+                                                                            // и заполнения числами
+{
+    int[,] newArray = new int[rows, columns];
+
+    for(int i = 0; i < rows; i++)
+        for(int j = 0; j < columns; j++)
+            newArray[i,j] = i + j;
+
+    return newArray;
+}
+
+void Show2dArray(int[,] array) // метод вывода на экран полученного 2D (двумерного) массива
+{
+    for(int i = 0; i < array.GetLength(0); i++) // GetLength - получить длину
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+            Console.Write(array[i,j] + " ");
+
+        Console.WriteLine();
+    }
+} 
+
+Console.WriteLine("Введите кол-во строк ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите кол-во колонок ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int[,] myArray = CreateRandom2dArray(m, n, 1, 9);
+Show2dArray(myArray); */
+
+// Задача 2. Задайте двумерный массив. Найдите элементы, у которых оба индекса чётные, 
+// и замените эти элементы на их квадраты.
+
+
+
 
